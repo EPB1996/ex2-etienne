0c673c0410407bb4fef99c1b146b9a7267cba5e4
Etienne Baumgartner <baume96@gmail.com>
2023-09-25 17:31:21 +0200

    Navbar and components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+import { ListeComponent } from './liste/liste.component';
+import { GestionComponent } from './gestion/gestion.component';
 
-const routes: Routes = [];
+const routes: Routes = [ { path: 'accueil', component: AcceuilComponent  },
+{ path: 'liste', component: ListeComponent },
+{ path: 'gestion', component: GestionComponent },
+// Add more routes for other components as needed
+{ path: '', redirectTo: '/accueil', pathMatch: 'full' }, // Default route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+import { ListeComponent } from './liste/liste.component';
+import { GestionComponent } from './gestion/gestion.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HeaderComponent, AcceuilComponent, ListeComponent, GestionComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
